19502fa44bc1aaa0279bd3179d948cf08c228b7a
dinarfathrahmanov <dinfat@mail.ru>
2023-06-18 16:58:35 +0300

    готово

---
+++ src/main/java/exceprions/DivizionByZero.java
@@ -0,0 +1,18 @@
+package exceprions;
+
+public class DivizionByZero extends IllegalArgumentException{
+    public DivizionByZero() {
+    }
+
+    public DivizionByZero(String s) {
+        super(s);
+    }
+
+    public DivizionByZero(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DivizionByZero(Throwable cause) {
+        super(cause);
+    }
+}
